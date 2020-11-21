955b743aee195f9551f73a4c07fc61bce7248348
agusp-dev <perezp.agustin@gmail.com>
2020-11-21 20:08:38 -0300

    Integrated simple routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+import { Home, Contact, About } from './components'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Routing Trainning 1</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <div className='container'>
+          {/* <h1 className='text-center'>React Routing Trainning 1</h1>
+          <hr/> */}
+          <nav className='navbar navbar-exapnd-lg navbar-light bg-light'>
+            <ul className="navbar-nav mr-auto flex-row">
+              <li className="nav-item col">
+                <Link
+                  className='nav-link'
+                  to='/'
+                  >Home
+                </Link>
+              </li>
+              <li className="nav-item col">
+                <Link
+                  className='nav-link'
+                  to='/contact'
+                  >Contact
+                </Link>
+              </li>
+              <li className="nav-item col">
+                <Link
+                  className='nav-link'
+                  to='/about'
+                  >About
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route 
+              exact 
+              path='/'
+              component={ Home } 
+            />
+            <Route 
+              exact 
+              path='/contact'
+              component={ Contact } 
+            />
+            <Route 
+              exact 
+              
+              path='/about'
+              component={ About } 
+            />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
